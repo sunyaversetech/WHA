bd1003b1415bd02ce5dfda2a708fa0149541879d
Aman Dhungel <aman.most100@gmail.com>
2026-03-23 22:24:27 +0545

    done with aws image error

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,12 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "wha-sunya-my-uploads.s3.ap-southeast-2.amazonaws.com",
+        pathname: "/**",
       },
     ],
   },
